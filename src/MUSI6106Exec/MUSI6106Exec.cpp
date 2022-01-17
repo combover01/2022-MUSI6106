dbbd893036e17ad9f1059c9de0b6ca761076d583
Mir Jeffres <smjeff515@gmail.com>
2022-01-16 19:07:09 -0500

    changes for exercise 1

--- src/MUSI6106Exec/MUSI6106Exec.cpp
+++ src/MUSI6106Exec/MUSI6106Exec.cpp
@@ -24,6 +24,7 @@ int main(int argc, char* argv[])
     clock_t                 time = 0;
 
     float                   **ppfAudioData = 0;
+    float** ppfAudioBuffer = 0;
 
     CAudioFileIf            *phAudioFile = 0;
     std::fstream            hOutputFile;
@@ -33,22 +34,81 @@ int main(int argc, char* argv[])
 
     //////////////////////////////////////////////////////////////////////////////
     // parse command line arguments
- 
+
+    if (argc == 3) {
+        sInputFilePath = argv[argc - 2];
+        sOutputFilePath = argv[argc - 1];
+    }
+    else if (argc == 2) {
+        sInputFilePath = argv[argc - 1];
+        sOutputFilePath = "outputText.txt";
+    }
+    else {
+        // default behavior: take last argument as the output file name
+        printf("Usage: \nMUSI6106Exec.exe <\"Audio File Path\"> [\"Output File Path\"]");
+        return -1;
+    }
     //////////////////////////////////////////////////////////////////////////////
     // open the input wave file
- 
+    //new //array, allocation is done in a loop. know your type
+    
+
+    stFileSpec.eFormat = CAudioFileIf::kFileFormatWav;
+    stFileSpec.eBitStreamType = CAudioFileIf::kFileBitStreamFloat32;
+    stFileSpec.iNumChannels = 2;
+    stFileSpec.fSampleRateInHz = 44100;
+
+    CAudioFileIf::create(phAudioFile);
+    phAudioFile->openFile(sInputFilePath, CAudioFileIf::kFileRead, &stFileSpec);
+    bool isOpenq = phAudioFile->isOpen();
+    bool isInit = phAudioFile->isInitialized();
+    //file.CAudioFileIf::openFile(sInputFilePath, CAudioFileIf::kFileFormatWav, stFileSpec) = 0;
+    //Error_t err = CAudioFileIf::openFile(filePth, CAudioFileIf::FileFormat_t::kFileFormatWav);
+    
+    //Error_t openFile(sInputFilePath);
+
     //////////////////////////////////////////////////////////////////////////////
     // open the output text file
+    std::ofstream outputText{ sOutputFilePath};
+
  
     //////////////////////////////////////////////////////////////////////////////
     // allocate memory
- 
+    // we need to find size of the audio file 
+    long long iLengthInFrames;
+    phAudioFile->getLength(*&iLengthInFrames);
+    const long long frameCount = iLengthInFrames;
+    const int channelCount = stFileSpec.iNumChannels;
+
+    ppfAudioData = new float* [channelCount]; //columns
+    for (int i = 0; i < channelCount; ++i) {
+        ppfAudioData[i] = new float[kBlockSize]; //rows. only 1024 samples every time
+    }
+    
     //////////////////////////////////////////////////////////////////////////////
     // get audio data and write it to the output text file (one column per channel)
 
+    long long readAmount = kBlockSize; //should read 1024 samples every time
+    while (readAmount == kBlockSize) {
+        phAudioFile->readData(ppfAudioData, readAmount);
+        for (int k = 0; k < readAmount; ++k) {
+            for (int j = 0; j < channelCount; ++j) {
+                std::string curData = std::to_string(ppfAudioData[j][k]) + "\t";
+                outputText << curData;
+            }
+            outputText << "\n";
+        }
+
+    }
+
+
+
+
+
     //////////////////////////////////////////////////////////////////////////////
     // clean-up (close files and free memory)
-
+    phAudioFile->closeFile();
+    CAudioFileIf::destroy(phAudioFile);
     // all done
     return 0;
 
