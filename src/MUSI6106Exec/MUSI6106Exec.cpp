61efd5530b22b78c85deee800de92e1e22561ecd
Mir Jeffres <smjeff515@gmail.com>
2022-02-13 23:53:03 -0500

    added processing to main exec

--- src/MUSI6106Exec/MUSI6106Exec.cpp
+++ src/MUSI6106Exec/MUSI6106Exec.cpp
@@ -12,6 +12,7 @@ using std::endl;
 
 // local function declarations
 void    showClInfo ();
+int processFilterExec(std::string sInputFilePath, std::string sOutputFilePath, CCombFilterIf::CombFilterType_t filterType, float fDelayInSec, float fGain, int blockSize);
 
 /////////////////////////////////////////////////////////////////////////////////
 // main function
@@ -20,30 +21,64 @@ int main(int argc, char* argv[])
     std::string sInputFilePath,                 //!< file paths
                 sOutputFilePath;
 
+    CCombFilterIf::CombFilterType_t filterType;
+    CCombFilterIf* phCombFilter = 0;
+    // filter params:
+    float fDelayInSec = 0;
+    float fGain = 1;
+    float fMaxDelayInSec = 1; // USER OF THE INTERFACE CAN DEFINE THIS, NOT USER OF EXECUTABLE
+
     static const int kBlockSize = 1024;
 
     clock_t time = 0;
 
     float **ppfAudioData = 0;
+    float** ppfAudioOutputData = 0;
+
 
     CAudioFileIf *phAudioFile = 0;
-    std::fstream hOutputFile;
+    CAudioFileIf *phOutputAudioFile = 0;
     CAudioFileIf::FileSpec_t stFileSpec;
 
     showClInfo();
 
+    
+
+
     //////////////////////////////////////////////////////////////////////////////
     // parse command line arguments
-    if (argc < 2)
+    //input path, filer type. delay time, gain
+    if (argc != 5 && argc != 1)
     {
-        cout << "Missing audio input path!";
+        cout << "Usage: \<input path\>, \<filter type ('FIR' or 'IIR')\>, \<delay time\>, \<gain\>";
         return -1;
+    }
+    else if (argc == 1) {
+        //run tests
+        cout << "Running tests" << "\n";
     }
     else
     {
         sInputFilePath = argv[1];
-        sOutputFilePath = sInputFilePath + ".txt";
+        sOutputFilePath = sInputFilePath + "_combFiltered.wav";
+
+        if (strcmp(argv[2], "FIR")) {
+            filterType = CCombFilterIf::CombFilterType_t::kCombFIR;
+        }
+        else if (strcmp(argv[2], "IIR")) {
+            filterType = CCombFilterIf::CombFilterType_t::kCombIIR;
+        }
+        else {
+            cout << "Incorrect filter type, possible types are 'FIR' or 'IIR'" << "\n";
+        }
+
+        fDelayInSec = atof(argv[3]);
+        fGain = atof(argv[4]);
+        
     }
+
+
+
 
     //////////////////////////////////////////////////////////////////////////////
     // open the input wave file
@@ -57,15 +92,7 @@ int main(int argc, char* argv[])
     }
     phAudioFile->getFileSpec(stFileSpec);
 
-    //////////////////////////////////////////////////////////////////////////////
-    // open the output text file
-    hOutputFile.open(sOutputFilePath.c_str(), std::ios::out);
-    if (!hOutputFile.is_open())
-    {
-        cout << "Text file open error!";
-        CAudioFileIf::destroy(phAudioFile);
-        return -1;
-    }
+
 
     //////////////////////////////////////////////////////////////////////////////
     // allocate memory
@@ -73,23 +100,25 @@ int main(int argc, char* argv[])
     for (int i = 0; i < stFileSpec.iNumChannels; i++)
         ppfAudioData[i] = new float[kBlockSize];
 
-    if (ppfAudioData == 0)
-    {
-        CAudioFileIf::destroy(phAudioFile);
-        hOutputFile.close();
-        return -1;
-    }
-    if (ppfAudioData[0] == 0)
-    {
-        CAudioFileIf::destroy(phAudioFile);
-        hOutputFile.close();
-        return -1;
-    }
+    //if (ppfAudioData == 0)
+    //{
+    //    CAudioFileIf::destroy(phAudioFile);
+    //    hOutputFile.close();
+    //    return -1;
+    //}
+    //if (ppfAudioData[0] == 0)
+    //{
+    //    CAudioFileIf::destroy(phAudioFile);
+    //    hOutputFile.close();
+    //    return -1;
+    //}
 
-    time = clock();
+    //time = clock();
 
-    //////////////////////////////////////////////////////////////////////////////
-    // get audio data and write it to the output text file (one column per channel)
+
+
+    ////////////////////////////////////////////////////////////////////////////////
+    //// get audio data and write it to the output text file (one column per channel)
     while (!phAudioFile->isEof())
     {
         // set block length variable
@@ -98,25 +127,23 @@ int main(int argc, char* argv[])
         // read data (iNumOfFrames might be updated!)
         phAudioFile->readData(ppfAudioData, iNumFrames);
 
-        cout << "\r" << "reading and writing";
+        // filter the audio
+        phCombFilter->process(ppfAudioData, ppfAudioOutputData, iNumFrames);
 
-        // write
-        for (int i = 0; i < iNumFrames; i++)
-        {
-            for (int c = 0; c < stFileSpec.iNumChannels; c++)
-            {
-                hOutputFile << ppfAudioData[c][i] << "\t";
-            }
-            hOutputFile << endl;
-        }
+        // write to output file path
+        phOutputAudioFile->writeData(ppfAudioOutputData, iNumFrames);
     }
 
-    cout << "\nreading/writing done in: \t" << (clock() - time) * 1.F / CLOCKS_PER_SEC << " seconds." << endl;
+    cout << "\nreading/filtering/writing done in: \t" << (clock() - time) * 1.F / CLOCKS_PER_SEC << " seconds." << endl;
+
+
+
 
     //////////////////////////////////////////////////////////////////////////////
     // clean-up (close files and free memory)
     CAudioFileIf::destroy(phAudioFile);
-    hOutputFile.close();
+    CAudioFileIf::destroy(phOutputAudioFile);
+    CCombFilterIf::destroy(phCombFilter);
 
     for (int i = 0; i < stFileSpec.iNumChannels; i++)
         delete[] ppfAudioData[i];
@@ -124,9 +151,29 @@ int main(int argc, char* argv[])
     ppfAudioData = 0;
 
     // all done
-    return 0;
+    return processFilterExec(sInputFilePath,sOutputFilePath,filterType,fDelayInSec,fGain,kBlockSize);
 
 }
+
+int processFilterExec(std::string sInputFilePath, std::string sOutputFilePath, CCombFilterIf::CombFilterType_t filterType, float fDelayInSec, float fGain, int blockSize) {
+    //The command line application instantiates your class with the correct parameters, 
+    //read the parameters from the command line(file name, effect - parameters), 
+    //reads and processes audio data in blocksand writes the result to an output wav file.
+
+    //instantiate the class with the correct parameters:
+
+
+
+
+
+
+
+
+    return 0;
+}
+
+
+
 
 
 void     showClInfo()
